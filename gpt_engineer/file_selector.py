7f7629e444c1280ab0105f4efd279de2e748d19f
harshitjain04 <harshit.jain04@nagarro.com>
2023-09-22 06:29:49 +0000

    ask for files code modified

--- gpt_engineer/file_selector.py
+++ gpt_engineer/file_selector.py
@@ -284,27 +284,24 @@ def ask_for_files(db_input) -> None:
 
     if not selection_number == 3:
         db_input[FILE_LIST_NAME] = "\n".join(file_path_list)
+       
 
 
 def ask_for_files_checkov(db_input):
-    directory_path='/tmp/helloworld'
+    directory_path='/tmp/gptWorkspace'
     file_list = []
-    file_dict = {}
     # Check if the directory exists
     if not os.path.exists(directory_path):
         raise ValueError(f"The directory '{directory_path}' does not exist.")
     
     # Iterate over files in the directory
-    for root, _, files in os.walk(directory_path):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            # Use relative file name as the key
-            relative_file_name = os.path.relpath(file_path, directory_path)
-            file_dict[relative_file_name] = file_path
-    
-    file_list.append(file_dict)
+    for item in os.listdir(directory_path):
+        item_path = os.path.join(directory_path, item)
+        if os.path.isfile(item_path):
+            file_list.append(item_path)
+
     db_input[FILE_LIST_NAME] = "\n".join(file_list)
-    return file_list
+    return []
 
 
 def gui_file_selector() -> List[str]:
